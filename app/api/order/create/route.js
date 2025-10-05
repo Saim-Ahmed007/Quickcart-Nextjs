e3d5f5a65f0204c67b89bfc72f8d03c854b33a33
Saim-Ahmed007 <saimahmed.seu.cse@gmail.com>
2025-10-06 00:50:59 +0600

    final updation

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -4,7 +4,7 @@ import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function ProductList(request) {
+export async function POST(request) {
     try {
         const {userId} = getAuth(request)
         const {address, items} = await request.json()
@@ -14,7 +14,7 @@ export async function ProductList(request) {
         //calculate amount using items
         const amount = await items.reduce(async(acc, item) =>{
             const product = await Product.findById(item.product)
-            return acc + product.offerPrice * item.quantity
+            return await acc + product.offerPrice * item.quantity
         },0)
         await inngest.send({
             name: 'order/created',
--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
 
 const orderSchema = new mongoose.Schema({
-    userId: {type: String, required: true},
+    userId: {type: String, required: true, ref: 'User'},
     items: [{
         product: {type: String, required: true, ref: 'Product'},
         quantity: {type: Number, required: true}
     }],
     amount: {type: String, required: true},
-    address: {type: String, required: true},
+    address: { type: String, ref: 'Address', required: true },
     status: {type: String, required: true, default: 'Order-Placed'},
     date: {type: Number, required: true},
 })
